29f2bfac6673266964d87fe6573447f0fc50191f
joonas kallinen <joonas.kallinen@student.jyu.fi>
2019-09-28 23:33:38 +0300

    Tehty skriptit ajoa ja testausta varten
    npm start ja npm test skriptit tehty, ja otettu jest-kirjasto mukaan testausta varten

--- tiea207-frontend/src/hooks/UseFetch.jsx
+++ tiea207-frontend/src/hooks/UseFetch.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 // Toteuttaa get-pyynnöt. Ei virheiden käsittelyä toistaiseksi.
+
 function useFetch(url) {
   const [data, setData] = useState([]);
-
+//const [isError, setIsError] = useState(false);
   useEffect(() => {
       const fetchData = async () => {
         const response = await axios.get(url);
