fc8dbe353e30bf495846ec21370ebbccf1c5d5a2
joonas kallinen <joonas.kallinen@student.jyu.fi>
2019-10-02 17:11:25 +0300

    Frontend: toimiva navigointipalkki
    Otettu käyttöön react-router-dom ja react-router-bootstrap kirjastot.
    Navigointipalkkiin tehty NavbarLink objekti, joka keskustelee react routerin kanssa.
    NavbarLinkiä käyttäessä sivun containeriin päivittyy klikatun sivun tiedot,
    eli toimii kuten tavallinen sivu, paitsi uudelleenlatausta ei tehdä.

--- tiea207-frontend/src/App.js
+++ tiea207-frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import TestContent from './components/TestContent'
 import { Container } from "react-bootstrap";
-import Navigation from './components/Navigation';
+import Navigation from './components/Navigation/Navigation';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Container>
+
+        <Router>
         <Navigation />
-        <Home />
+            <Route exact path="/" component={Home} />
+            <Route path="/test" component={TestContent} />
+        </Router>
       </Container>
     </div>
   );
