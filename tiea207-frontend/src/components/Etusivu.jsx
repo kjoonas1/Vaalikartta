42110f3bad0975be51a0a19d841a9c315aa43606
kjoonas1 <joonas.kallinen@student.jyu.fi>
2019-10-15 17:29:50 +0300

    Timeline komponentti luotu
    Tehty timeline komponentti ja jaettu etusivun
    koodista osa ElectionMap komponentille.
    Timeout laitettu usefetchiin.

--- tiea207-frontend/src/components/Etusivu.jsx
+++ tiea207-frontend/src/components/Etusivu.jsx
@@ -1,54 +1,31 @@
 
 import React, { Fragment, useContext } from "react"
-import { Map, GeoJSON } from "react-leaflet"
 import { Col, Row } from "react-bootstrap"
 import { useFetch } from "../hooks/UseFetch"
-import { ReactComponent as Timeline } from "../vuosijana.svg"
+import { Timeline } from "./Timeline"
 import chart from "../chart.png"
 import { AreaContext } from "../Contexts"
-
+import { ElectionMap } from "./ElectionMap"
 
 const Etusivu = () => {
 
     const { area, setArea } = useContext(AreaContext)
-    
-    // hoitaa kartan klikkauksen
-    const addAreaInfo = (feature, layer) => {
-        if (feature.properties && feature.properties.name) {
-            layer.on({
-                click: (event) => setArea(event.target.feature.properties.name)
-            })
-        }
-    }
-    
     const mapData = useFetch("http://localhost:8000/api/maps/municipalityborders")
     const areaData = useFetch("http://localhost:8000/api/districts/district/", {district: area})
+    const years = [1983, 1987, 1991, 1995, 1999, 2003, 2007, 2011, 2015, 2019]
 
-    console.log(areaData)
-    if (mapData.isLoading) {
+    if (mapData.isLoading) 
         return <div>Loading map data...</div>
-    }
+    
     if (!mapData.isLoading && mapData.error === null) {
         return (
             <Fragment>
                 <Row className="timeline">
-                    <Timeline width="100%" />
+                    <Timeline years={years} />
                 </Row>
                 <Row>
                     <Col xs={12} xl={4}>
-                        <Map center={[65.1, 25.489]} dragging={false} preferCanvas={true}  zoom={5}>
-                            <GeoJSON
-                                keyFunction={mapData.data}
-                                data={mapData.data}
-                                onEachFeature={addAreaInfo}
-                                style={() => ({
-                                    color: "#4a83ec",
-                                    weight: 0.75,
-                                    fillColor: "#18447e",
-                                    fillOpacity: 1
-                                })}
-                            />
-                        </Map>
+                        <ElectionMap mapData={mapData}/>
                     </Col>
                     <Col xs={12} xl={8}>
                         <p>{area}</p>
