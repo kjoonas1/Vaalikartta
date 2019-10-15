7ffdd32f992a3b0ee3754d5e8df44e7795c5d6e6
kjoonas1 <joonas.kallinen@student.jyu.fi>
2019-10-15 17:36:27 +0300

    Lisätty timeline erillisenä komponenttina

---
+++ tiea207-frontend/src/components/Timeline.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react"
+import { Col } from "react-bootstrap"
+import shortid from "shortid"
+import {Link} from "react-router-dom"
+
+export const Timeline = (props) => {
+    
+const SVG_WIDTH = 50; // kerroin
+
+return (
+        <Col md={{ span: 10, offset: 1 }}>
+            <svg key={shortid.generate()} height="10rem" width={"65vw"} display= "block" margin="0, auto">
+                <line key={shortid.generate()} x1="2vw" y1="1rem" x2={"52vw"} y2="1rem" style={ {stroke:"rgb(15,15,15)", strokeWidth:"5"} } />
+                { props.years.map((year, index) => {
+                    const x = 2+(SVG_WIDTH*(index/(props.years.length-1)))
+                    const lineX = x.toString() + "vw"
+                    return (
+                        <Fragment key={shortid.generate()}>
+                            <line key={shortid.generate()} x1={lineX} y1="-25" x2={lineX} y2="25" style={ {stroke:"rgb(15,15,15)", strokeWidth:"5"} } />
+                            <Link to="/#" onClick={() => {
+                                console.log(year)
+                            }}>
+                                <text textAnchor="middle" key={shortid.generate()} x={lineX} y="3rem" fill="black">{year}</text>
+                            </Link>
+                        </Fragment>
+                    )
+                })}
+            </svg>
+        </Col>
+    )
+}
