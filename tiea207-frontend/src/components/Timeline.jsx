74a03f81605cd7824e83cefba6c2572697b97ed7
kjoonas1 <joonas.kallinen@student.jyu.fi>
2019-10-16 14:21:35 +0300

    Säädetty timelineä

--- tiea207-frontend/src/components/Timeline.jsx
+++ tiea207-frontend/src/components/Timeline.jsx
@@ -8,19 +8,20 @@ export const Timeline = (props) => {
     
     const SVG_WIDTH = 50 // kerroin
     const { setYear } = useContext(YearContext)
+    const years = props.data.years
 
     return (
         <Col md={{ span: 10, offset: 1 }}>
             <svg key={shortid.generate()} height="10rem" width={"65vw"} display= "block" margin="0, auto">
-                <line key={shortid.generate()} x1="2vw" y1="1rem" x2={"52vw"} y2="1rem" style={ {stroke:"rgb(15,15,15)", strokeWidth:"5"} } />
-                { props.years.map((year, index) => {
-                    const x = 2+(SVG_WIDTH*(index/(props.years.length-1)))
+                <line key={shortid.generate()} x1="2vw" y1="1rem" x2={"52vw"} y2="1rem" style={ {stroke:"#404040", strokeWidth:"0.25rem"} } />
+                { years.map((year, index) => {
+                    const x = 2+(SVG_WIDTH*(index/(years.length-1)))
                     const lineX = x.toString() + "vw"
                     return (
                         <Fragment key={shortid.generate()}>
                             <Link to="/#" onClick={() => { setYear(year)} }>
-                                <line key={shortid.generate()} x1={lineX} y1="-25" x2={lineX} y2="25" style={ {stroke:"rgb(15,15,15)", strokeWidth:"5"} } />
-                                <text textAnchor="middle" key={shortid.generate()} x={lineX} y="3rem" fill="black">{year}</text>
+                                <line key={shortid.generate()} x1={lineX} y1="0.5rem" x2={lineX} y2="2rem" style={ {stroke:"#404040", strokeLinecap:"round", strokeWidth:"0.25rem"} } />
+                                <text textAnchor="middle"  key={shortid.generate()} x={lineX} y="3rem" fill="#404040">{year}</text>
                             </Link>
                         </Fragment>
                     )
