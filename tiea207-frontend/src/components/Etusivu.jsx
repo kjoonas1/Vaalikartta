f59cb6009af4628077b1bfae0451ac338c959fe0
kjoonas1 <joonas.kallinen@student.jyu.fi>
2019-10-16 16:54:10 +0300

    Timelinelle paikka datapallukoille

--- tiea207-frontend/src/components/Etusivu.jsx
+++ tiea207-frontend/src/components/Etusivu.jsx
@@ -15,7 +15,17 @@ const Etusivu = () => {
     const mapData = useFetch("http://localhost:8000/api/maps/municipalityborders")
     // const areaData = useFetch("http://localhost:8000/api/districts/district/", {district: area})
     const timelineData = {
-        years: [1983, 1987, 1991, 1995, 1999, 2003, 2007, 2011, 2015, 2019]
+        years: [1983, 1987, 1991, 1995, 1999, 2003, 2007, 2011, 2015, 2019],
+        events: [
+            {
+                name: "Neuvostoliiton hajoaminen",
+                year: 1991
+            },
+            {
+                name: "Testitesti",
+                year: 2006
+            }
+        ]
         
     }
 
