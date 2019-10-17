65aa323b8f7fb063249c11286c934541bcb48102
kjoonas1 <joonas.kallinen@student.jyu.fi>
2019-10-17 10:39:43 +0300

    Eventin aktiivisuus näkyy aikajanalla
    Valittu event näkyy kirkkaana aikajanalla.
    Tehty eventille oma konteksti ja vähennetty
    kovakoodausta svg:stä

--- tiea207-frontend/src/components/Timeline.jsx
+++ tiea207-frontend/src/components/Timeline.jsx
@@ -2,22 +2,27 @@ import React, { useContext, Fragment } from "react"
 import { Col } from "react-bootstrap"
 import shortid from "shortid"
 import {Link} from "react-router-dom"
-import { YearContext } from "../Contexts"
+import { YearContext, EventContext } from "../Contexts"
 
 export const Timeline = (props) => {
     
     const { year, setYear } = useContext(YearContext)
+    const { event, setEvent } = useContext(EventContext)
+
     const years = props.data.years
     const events = props.data.events
-    const contextYear = year;
+
+    const contextYear = year
+    const contextEvent = event
+    const padding = 10
 
     return (
         <Col md={{ span: 12 }}>
             <svg key={shortid.generate()} height="14em" viewBox={"0 0 2400 400"} width={"100%"} display= "block" margin="0, auto">
-                <line key={shortid.generate()} x1="10%" y1="6em" x2={"90%"} y2="6em" style={ {stroke:"#404040", strokeWidth:"0.5em"} } />
+                <line key={shortid.generate()} x1={padding + "%"} y1="6em" x2={(100-padding) + "%"} y2="6em" style={ {stroke:"#404040", strokeWidth:"0.5em"} } />
                 { years.map((year, index) => {
-                    const x = (80*(index/(years.length-1)))
-                    const lineX = (10 + x).toString()
+                    const x = ((100-2*padding)*(index/(years.length-1))) // Vaakaviivojen ja tekstin paikka, jossa ei vielä huomioida paddingia
+                    const lineX = (padding + x).toString()
                     const bolding = contextYear === year ? "bold" : "normal"
                     return (
                         <Fragment key={shortid.generate()}>
@@ -28,17 +33,23 @@ export const Timeline = (props) => {
                         </Fragment>
                     )
                 })}
-                { events.map((event, index) => {
+                { events.map((event) => {
                     const minYear = Math.min(...years)
                     const maxYear = Math.max(...years)
-                    const x = 10+(80*(event.year-minYear)/(maxYear - minYear))
+                    const x = padding+(100-2*padding)*(event.year-minYear)/(maxYear - minYear)
+                    const isActive = JSON.stringify(event) === JSON.stringify(contextEvent)
+                    const eventActiveness = {
+                        fill: isActive ? "1" : "0.25",
+                        size: isActive ? "1.5em" : "1em"
+                    }
                     return (
-                    <Fragment key={shortid.generate()}>
-                        <Link to="/#">
-                            <line key={shortid.generate()} x1={(x) + "%"} y1="6em" x2={(x) + "%"} y2="8em" style={ {stroke:"#404040", strokeLinecap:"round", strokeWidth:"0.25em"} } />
-                            <circle stroke="black" fillOpacity="0.25" strokeWidth="2" fill="#fcb103" cx={x+ "%"} cy="9em" r="1em" ></circle>
-                        </Link>
-                    </Fragment>)
+                        <Fragment key={shortid.generate()}>
+                            <Link to="/#" onClick={() => { setEvent(event)} }>
+                                <line key={shortid.generate()} x1={(x) + "%"} y1="6em" x2={(x) + "%"} y2="8em" style={ {stroke:"#404040", strokeLinecap:"round", strokeWidth:"0.25em"} } />
+                                <circle stroke="black" fillOpacity={eventActiveness.fill} strokeWidth="2" fill="#fcb103" cx={x+ "%"} cy="9em" r={eventActiveness.size} ></circle>
+                            </Link>
+                        </Fragment>
+                    )
                 })}
             </svg>
         </Col>
