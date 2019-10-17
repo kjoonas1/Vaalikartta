e08d73ebfa0fd1973e34249085dca321e24fb6b9
kjoonas1 <joonas.kallinen@student.jyu.fi>
2019-10-17 10:49:46 +0300

    Vähennetty kovakoodausta
    Lisätty muuttujia ja vahvistettu hieman
    valitun eventin erottuvuutta

--- tiea207-frontend/src/components/Timeline.jsx
+++ tiea207-frontend/src/components/Timeline.jsx
@@ -15,11 +15,11 @@ export const Timeline = (props) => {
     const contextYear = year
     const contextEvent = event
     const padding = 10
+    const mainLineHeight = "6em"
 
     return (
         <Col md={{ span: 12 }}>
             <svg key={shortid.generate()} height="14em" viewBox={"0 0 2400 400"} width={"100%"} display= "block" margin="0, auto">
-                <line key={shortid.generate()} x1={padding + "%"} y1="6em" x2={(100-padding) + "%"} y2="6em" style={ {stroke:"#404040", strokeWidth:"0.5em"} } />
                 { years.map((year, index) => {
                     const x = ((100-2*padding)*(index/(years.length-1))) // Vaakaviivojen ja tekstin paikka, jossa ei vielä huomioida paddingia
                     const lineX = (padding + x).toString()
@@ -27,7 +27,7 @@ export const Timeline = (props) => {
                     return (
                         <Fragment key={shortid.generate()}>
                             <Link key={shortid.generate()} to="/#" onClick={() => { setYear(year)} }>
-                                <line key={shortid.generate()} x1={(lineX) + "%"} y1="6em" x2={(lineX) + "%"} y2="4em" style={ {stroke:"#404040", strokeLinecap:"round", strokeWidth:"0.5em"} } />
+                                <line key={shortid.generate()} x1={(lineX) + "%"} y1={mainLineHeight} x2={(lineX) + "%"} y2="4em" style={ {stroke:"#404040", strokeLinecap:"round", strokeWidth:"0.5em"} } />
                                 <text textAnchor="middle" fontSize="2.5em" key={shortid.generate()} x={lineX+ "%"} y="1em" fill="#404040" fontWeight={bolding}>{year}</text>
                             </Link>
                         </Fragment>
@@ -40,17 +40,19 @@ export const Timeline = (props) => {
                     const isActive = JSON.stringify(event) === JSON.stringify(contextEvent)
                     const eventActiveness = {
                         fill: isActive ? "1" : "0.25",
-                        size: isActive ? "1.5em" : "1em"
+                        size: isActive ? "1.5em" : "1em",
+                        borderColor: isActive ? "#404040" : "#757575"
                     }
                     return (
                         <Fragment key={shortid.generate()}>
                             <Link to="/#" onClick={() => { setEvent(event)} }>
-                                <line key={shortid.generate()} x1={(x) + "%"} y1="6em" x2={(x) + "%"} y2="8em" style={ {stroke:"#404040", strokeLinecap:"round", strokeWidth:"0.25em"} } />
-                                <circle stroke="black" fillOpacity={eventActiveness.fill} strokeWidth="2" fill="#fcb103" cx={x+ "%"} cy="9em" r={eventActiveness.size} ></circle>
+                                <line key={shortid.generate()} x1={(x) + "%"} y1={mainLineHeight} x2={(x) + "%"} y2="8em" style={ {stroke:eventActiveness.borderColor, strokeLinecap:"round", strokeWidth:"0.25em"} } />
+                                <circle stroke={eventActiveness.borderColor} fillOpacity={eventActiveness.fill} strokeWidth="2" fill="#fcb103" cx={x+ "%"} cy="9em" r={eventActiveness.size} ></circle>
                             </Link>
                         </Fragment>
                     )
                 })}
+                <line key={shortid.generate()} x1={padding + "%"} y1={mainLineHeight} x2={(100-padding) + "%"} y2={mainLineHeight} style={ {stroke:"#404040", strokeWidth:"0.5em"} } />
             </svg>
         </Col>
     )
