364e6e8043e575b48240b74ea92f2dc390471e53
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2019-10-24 15:23:44 +0300

    Virheviestiksi vaihtunut 404
    Ilmeisesti `app.js`:ssä ei ehditä yhdistää Mongoon ennen kuin testit
    käynnistyvät.

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,41 +5,42 @@ const morgan = require("morgan")
 const MongoClient = require("mongodb").MongoClient
 const router = require("./routes/routes")
 
-if (process.env.TEST)
-    console.log = () => { } // Otetaan loggaus pois käytöstä jottei sotke testejä
+// if (process.env.TEST)
+//     console.log = () => { } // Otetaan loggaus pois käytöstä jottei sotke testejä
 
 // Käyttäjällä vaalikartta on vain lukuoikeus tietokantaan
 const databaseUrl = `mongodb+srv://vaalikartta:${process.env.PASSWD}@klusteri-asaca.mongodb.net/test?retryWrites=true&w=majority`
 const databaseName = "vaalikartta"
+process.on("exit", () => {
+    app.onExit()
+})
 
-var mongoClient = null
 MongoClient.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
-    mongoClient = client
+    app.mongoClient = client
     console.log("Connected to database")
+
+    app.use(cors())
+    app.use(morgan("tiny"))
+    // Middleware, joka vie Db instanssin jokaisen pyynnön mukana
+    app.use((req, res, next) => {
+        console.log("app.mongoClient: ", app.mongoClient)
+        console.log("db: ", req.db)
+        req.db = app.mongoClient.db(databaseName)
+        next()
+    })
+
+    app.use("/api", router)
+    // Tämä vain jotta /favicon.ico hakeminen ei tuota 404
+    app.get("/favicon.ico", (req, res) => res.sendStatus(204))
+    app.get("/", (req, res) => res.send("Hello"))
 }).catch(err => {
     console.log("Error connecting to database", err)
     process.exit(1)
 })
 
-app.use(cors())
-// app.use(morgan("tiny"))
-// Middleware, joka vie Db instanssin jokaisen pyynnön mukana
-app.use((req, res, next) => {
-    req.db = mongoClient.db(databaseName)
-    next()
-})
-
-// Tämä vain jotta /favicon.ico hakeminen ei tuota 404
-app.use("/api", router)
-app.get("/favicon.ico", (req, res) => res.sendStatus(204))
-
-process.on("exit", () => {
-    app.onExit()
-})
-
 app.onExit = () => {
-    if (mongoClient)
-        mongoClient.close()
+    if (app.mongoClient)
+        app.mongoClient.close()
 }
 
 module.exports = app
--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,14 +1,32 @@
 const supertest = require("supertest")
 const app = require("../src/app")
+const http = require("http")
+const express = require("express")
 
-const api = supertest(app)
-
-test("hello", () => {
-    expect(true).toBe(true)
-    app.onExit()
+let server
+let request
+let expApp
+beforeAll(done => {
+    expApp = express()
+    expApp.get("/", (res, req) => req.send(200))
+    server = http.createServer(app)
+    server.listen(done)
+    request = supertest(server)
+    
 })
 
-test("Vaalipiireistä saadaan dataa", () => {
-    api.get("/api/vaalipiirit/kannatus/Lapin vaalipiiri/2011")
-        .then(res => expect(res.status).toBe(200))
+afterAll(done => {
+    server.close(done)
+})
+
+test("sup", async (done) => {
+
+    const res = await request.get("/")
+    // console.log(res)
+    expect(res.status).toBe(200)
+})
+
+test("2", async done => {
+    const res = await request.get("/api/vaalipiirit/kannatus/Lapin vaalipiiri/2011")
+    expect(res.status).toBe(200)
 })
