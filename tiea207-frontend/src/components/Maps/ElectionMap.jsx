0f7f85a51850b7b280127ca81733e07ea478b130
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2019-11-07 15:38:20 +0200

    Kunnat kartalle leafletilla init

--- tiea207-frontend/src/components/Maps/ElectionMap.jsx
+++ tiea207-frontend/src/components/Maps/ElectionMap.jsx
@@ -1,23 +1,40 @@
 import React, { useContext } from "react"
-import { Map, GeoJSON } from "react-leaflet"
-import { AreaContext } from "../../contexts/Contexts"
+import { Map, GeoJSON, TileLayer } from "react-leaflet"
+import { AreaContext, YearContext } from "../../contexts/Contexts"
+import { useFetch } from "../../hooks/UseFetch"
+import { backendUrl } from "../../constants"
 
-export const ElectionMap = props => {
-    const { setArea } = useContext(AreaContext)
+export const ElectionMap = () => {
+    const { dispatchArea } = useContext(AreaContext)
+    const { year } = useContext(YearContext)
+    const { data, error, isLoading } = useFetch(`${backendUrl}/api/kunnat/koordinaatit/${year}`)
+    const mapData = data
     // hoitaa kartan klikkauksen
     const addAreaInfo = (feature, layer) => {
         if (feature.properties && feature.properties.name) {
             layer.on({
-                click: event => setArea(event.target.feature.properties.name)
+                click: () => {
+                    console.log(feature)
+                    dispatchArea({ type: "CHANGE_DISTRICT_TO", to: feature.properties.name })
+                }
             })
         }
     }
 
+    if (isLoading)
+        return <div>Loading...</div>
+    if (error)
+        return <div>Error!</div>
+
     return (
-        <Map center={[65.1, 25.489]} dragging={false} attributionControl={false} preferCanvas={true} zoom={5}>
+        <Map center={[64, 25]} dragging={true} attributionControl={false} preferCanvas={true} zoom={5}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
             <GeoJSON
-                keyFunction={props.mapData.data}
-                data={props.mapData.data}
+                keyFunction={mapData}
+                data={mapData}
                 onEachFeature={addAreaInfo}
                 style={() => ({
                     color: "#4a83ec",
