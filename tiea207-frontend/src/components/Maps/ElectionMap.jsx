e5acbab7edcaec2a30848a9eafd3a04fca07ce66
teejiipee <teemu.t.korhonen@student.jyu.fi>
2019-11-11 03:34:43 +0200

    Kunnat kartalle leafletilla
    Kartan markerit muutettu defaultista palleroiksi ja tooltipsit lisätty.
    Kartta on buginen siirryttäessä kuntien välilehdelle, mutta toimii oikein,
    kun vaihdetaan kerran vuotta. Kartta piirtyy myös oikein, jos sovellus starttaa kuntien
    välilehdeltä.

--- tiea207-frontend/src/components/Maps/ElectionMap.jsx
+++ tiea207-frontend/src/components/Maps/ElectionMap.jsx
@@ -3,18 +3,36 @@ import { Map, GeoJSON, TileLayer } from "react-leaflet"
 import { AreaContext, YearContext } from "../../contexts/Contexts"
 import { useFetch } from "../../hooks/UseFetch"
 import { backendUrl } from "../../constants"
+import L from "leaflet"
+
+//import 'leaflet/dist/leaflet.css'
+
 
 export const ElectionMap = () => {
     const { dispatchArea } = useContext(AreaContext)
     const { year } = useContext(YearContext)
     const { data, error, isLoading } = useFetch(`${backendUrl}/api/kunnat/koordinaatit/${year}`)
     const mapData = data
+
+    const pointToLayer = (feature, latlng) => {
+        console.log("feature: ", feature)
+        console.log("latlng: ", latlng)
+        return L.circleMarker(latlng, null)
+    }
+
+
+
     // hoitaa kartan klikkauksen
     const addAreaInfo = (feature, layer) => {
+        if (feature.properties && feature.properties.popupContent) {
+            layer.on({
+                mouseover: () => { layer.bindPopup(feature.properties.popupContent).openPopup() },
+                mouseout: () => { layer.bindPopup(feature.properties.popupContent).closePopup() }
+            })
+        }
         if (feature.properties && feature.properties.name) {
             layer.on({
                 click: () => {
-                    console.log(feature)
                     dispatchArea({ type: "CHANGE_DISTRICT_TO", to: feature.properties.name })
                 }
             })
@@ -27,19 +45,25 @@ export const ElectionMap = () => {
         return <div>Error!</div>
 
     return (
-        <Map center={[64, 25]} dragging={true} attributionControl={false} preferCanvas={true} zoom={5}>
+
+        //piirtyy nopeammin jostain syystä kun koko määritellään tässä
+        <Map style={{ width: "100%", height: "600px" }} center={[65.5, 25]} dragging={true} attributionControl={false} preferCanvas={true} zoom={5}>
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
             <GeoJSON
                 keyFunction={mapData}
                 data={mapData}
                 onEachFeature={addAreaInfo}
+                pointToLayer={pointToLayer}
+
                 style={() => ({
-                    color: "#4a83ec",
+                    radius: 5,
+                    color: "#000000",
                     weight: 0.75,
-                    fillColor: "#18447e",
+                    fillColor: "#097ab8",
                     fillOpacity: 1
                 })}
             />
