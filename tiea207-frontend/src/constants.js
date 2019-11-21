dce45f525a579c224a3ff3528564cdf1e3abf850
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2019-11-21 14:22:09 +0200

    Typon korjaus

--- tiea207-frontend/src/constants.js
+++ tiea207-frontend/src/constants.js
@@ -1,1 +1,1 @@
-export const backendUrl =  process.env.NODE_ENV === "production" ? "http://vaalipiirit.appspot.com" : "http://localhost:8000"
+export const backendUrl =  process.env.NODE_ENV === "production" ? "http://vaalikartta.appspot.com" : "http://localhost:8000"
