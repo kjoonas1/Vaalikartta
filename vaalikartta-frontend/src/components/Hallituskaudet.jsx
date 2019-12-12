dc490ef0f48042c993c195c13125a26c526e8edb
teejiipee <teemu.t.korhonen@student.jyu.fi>
2019-12-12 11:20:26 +0200

    Hallitustiedot päivitetty & kuvat kuntoon

--- vaalikartta-frontend/src/components/Hallituskaudet.jsx
+++ vaalikartta-frontend/src/components/Hallituskaudet.jsx
@@ -1,43 +1,38 @@
-import React, { useState } from "react"
+import React from "react"
 import "../styles/Hallitustiedot.scss"
-import { useQuery } from "react-fetching-library"
-import { Tab, Tabs, Accordion, Card, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap'
+import { Tab, Tabs, Accordion, Card, ListGroup, ListGroupItem, Row, Col } from "react-bootstrap"
 
 
 const Hallituskaudet = ({ data }) => {
 
-    const paaministerit = ["/sorsa.jpg", "/holkeri.jpg", "/aho.jpg", "/lipponen.jpg", "/lipponen.jpg", "/jaatteenmaki.jpg", "/vanhanen.jpg", "/vanhanen.jpg", "/kiviniemi.jpg", "/katainen.jpg", "/stubb.jpg", "/sipila.jpg", "/rinne.jpg"]
-
-    const [counter, setCounter] = useState(0)
-
-    let apu = parseInt(data.map(id => id.ID)) + counter
-    if (isNaN(apu))
-        apu = 0
 
     console.log("hallitusdata", data)
     if (data.length !== 0) {
-        return <Tabs>
+        return <Tabs defaultActiveKey="0">
             {
+                
                 data.map((hallitus, i) => {
-                    return <Tab eventKey={i} title={hallitus.Hallitus} className="hallitus-tab">
+                    console.log("tässä i ", i)
+                    console.log("tässä hallitus ", Tabs.activeKey)
+                    return <Tab key={hallitus._id} eventKey={i} title={hallitus.Hallitus} className="hallitus-tab">
                         <Row>
                             <Col md={4}>
                                 <Card key={"hallitus" + i}>
-                                    <Card.Img variant="top" className="paaministerikuva" src={paaministerit[apu - 2]} alt="vanhanen vain nauroi" />
+                                    <Card.Img variant="top" className="paaministerikuva" src={hallitus.Kuva} alt="vanhanen vain nauroi" />
                                     <Card.Body>
                                         <Card.Text>
                                             <ListGroup variant="flush">
-                                            <ListGroupItem><b>Alku: </b>{hallitus.Alku}</ListGroupItem>
-                                            <ListGroupItem><b>Loppu:</b> {hallitus.Loppu}</ListGroupItem>
-                                            <ListGroupItem><b>Hallituspäivät:</b> {hallitus["Hallituspäivät"]}</ListGroupItem>
-                                            <ListGroupItem><b>Pääministerin puolue:</b> {hallitus["Pääministerin puolue"]}</ListGroupItem>
+                                                <ListGroupItem><b>Alku: </b>{hallitus.Alku}</ListGroupItem>
+                                                <ListGroupItem><b>Loppu:</b> {hallitus.Loppu}</ListGroupItem>
+                                                <ListGroupItem><b>Hallituspäivät:</b> {hallitus["Hallituspäivät"]}</ListGroupItem>
+                                                <ListGroupItem><b>Pääministerin puolue:</b> {hallitus["Pääministerin puolue"]}</ListGroupItem>
                                             </ListGroup>
                                         </Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Col>
                             <Col md={8}>
-                                <Accordion>
+                                <Accordion className="ministerit">
                                     {hallitus.ministerit.map((nimi, i) =>
                                         <Card key={nimi._id}>
                                             <Accordion.Toggle as={Card.Header} eventKey={i}><b>{nimi.Rooli}</b> {": " + nimi.Ministeri}</Accordion.Toggle>
