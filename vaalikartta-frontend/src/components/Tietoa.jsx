8bd6272836de3150fcce68e249998e2aa6663504
Juuso <juuso45@hotmail.com>
2019-12-13 16:06:59 +0200

    Lisätty lähteeksi OpenStreetMap

--- vaalikartta-frontend/src/components/Tietoa.jsx
+++ vaalikartta-frontend/src/components/Tietoa.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Row, Table } from "react-bootstrap"
+import { Table } from "react-bootstrap"
 
 const Tietoa = () => {
     return <div>
@@ -37,7 +37,14 @@ const Tietoa = () => {
                     <td>31.10.2019</td>
                     <td><a href="https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606">CC BY 4.0</a></td>
                 </tr>
-
+                <tr>
+                    <td>Kuntakartan tausta, <a href="https://www.openstreetmap.org/copyright">Openstreetmap</a></td>
+                    <td></td>
+                    <td><a href="https://creativecommons.org/licenses/by-sa/2.0/" >CC BY-SA</a> </td>
+                </tr>
+                <p>
+                    Kuntien koordinaatit.Wikipedia.Aineisto on haettu 22.10.2019 lisenssillä CC BY SA 4.0. Kuvat.Wikipedia.Aineisto on haettu Wikipediasta lisenssillä CC BY 2.0.
+</p>
             </tbody>
         </Table>
         <h3>Dataan tehdyt muutokset</h3>
@@ -45,8 +52,6 @@ const Tietoa = () => {
             Tilastokeskukselta data ladattiin CSV-formaatissa ja siitä poistettiin tyhjiä kenttiä, minkä jälkeen se muutettiin JSON-formaattiin.
             </p>
         <p>
-            Kuntakartan tausta.OpenStreetMap.Aineisto on ladattu OpenStreetMapin
-            rajapintapalvelusta lisenssillä CC BY - SA https://www.openstreetmap.org/copyright.
             Kuntien koordinaatit.Wikipedia.Aineisto on haettu 22.10.2019 lisenssillä CC BY SA
             4.0. Kuvat.Wikipedia.Aineisto on haettu Wikipediasta lisenssillä CC BY 2.0.
 </p>
