1b656fd7fc86efc41e35bc4aedf35ea2e2b60c97
Juuso <juuso45@hotmail.com>
2019-12-13 15:04:51 +0200

    Tekijät ja lisää lähteitä kirjoitettu.

--- vaalikartta-frontend/src/components/Tietoa.jsx
+++ vaalikartta-frontend/src/components/Tietoa.jsx
@@ -1,16 +1,19 @@
 import React from "react"
 
 const Tietoa = () => {
-    return <>Jyväskylän Yliopiston Informaatioteknologiatiedekunnan TIEA207-kurssin projekti, jossa avointa dataa hyödyntäen on luotu web-sivusto, jossa voi katsoa interaktiivisen kartan avulla Suomen eduskuntavaalien tuloksia koko maan, vaalipiirien ja kuntien osalta vuodesta 1983 alkaen. Lisäksi kultakin vuodelta voi tarkastella hallituksen kokoonpanoa.
+    return <>Jyväskylän Yliopiston Informaatioteknologiatiedekunnan TIEA207 Aineopintojen projektikurssi, jossa avointa dataa hyödyntäen on luotu web-sivusto, jossa voi katsoa interaktiivisen kartan avulla Suomen eduskuntavaalien tuloksia koko maan, vaalipiirien ja kuntien osalta vuodesta 1983 alkaen. Lisäksi kultakin vuodelta voi tarkastella hallituksen kokoonpanoa.
     Projekti Githubissa: https://github.com/kjoonas1/Vaalikartta.
 
     Tämän projektin lisenssi on GPLv3.
 
     Data on haettu seuraavista lähteistä:
-    Eduskuntavaali- ja kuntatiedot. Tilastokeskus. Aineisto on ladattu Tilastokeskuksen rajapintapalvelusta .10.2019 lisenssillä  CC BY 4.0 https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606.
-    Kuntakartan tausta. OpenStreetMap. Aineisto on ladattu OpenStreetMapin rajapintapalvelusta  .2019 lisenssillä CC BY-SA https://www.openstreetmap.org/copyright.
-    Kuntien koordinaatit. Wikipedia. Aineisto on haettu .2019 lisenssillä CC BY SA 4.0.
-     </>
+    Eduskuntavaali- ja kuntatiedot. Tilastokeskus. Aineisto on ladattu Tilastokeskuksen rajapintapalvelusta lisenssillä  CC BY 4.0 https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606. Äänestystiedot ja puoluekannatus ladattiin 18.10.2019, ja  kuntien avainluvut ladattiin 31.10.2019. Data on muunnettu CSV-formaatista JSON-formaattiin ja tyhjiä kenttiä poistettu.
+    Kuntakartan tausta. OpenStreetMap. Aineisto on ladattu OpenStreetMapin rajapintapalvelusta lisenssillä CC BY-SA https://www.openstreetmap.org/copyright.
+    Kuntien koordinaatit. Wikipedia. Aineisto on haettu 22.10.2019 lisenssillä CC BY SA 4.0.
+    Kuvat. Wikipedia. Aineisto on haettu Wikipediasta lisenssillä CC BY 2.0.
+
+    Tekijät: Juuso Huurinainen, Aleksi Tarvainen, Joonas Kallinen, Teemu Korhonen
+    </>
 }
 
 export default Tietoa
