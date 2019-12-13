fb86a032d7156abdf3d7d64912d6b365c53bbb74
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2019-12-13 15:43:02 +0200

    Tietoa sivun pohja WIP

--- vaalikartta-frontend/src/components/Tietoa.jsx
+++ vaalikartta-frontend/src/components/Tietoa.jsx
@@ -1,19 +1,65 @@
 import React from "react"
+import { Row, Table } from "react-bootstrap"
 
 const Tietoa = () => {
-    return <>Jyväskylän Yliopiston Informaatioteknologiatiedekunnan TIEA207 Aineopintojen projektikurssi, jossa avointa dataa hyödyntäen on luotu web-sivusto, jossa voi katsoa interaktiivisen kartan avulla Suomen eduskuntavaalien tuloksia koko maan, vaalipiirien ja kuntien osalta vuodesta 1983 alkaen. Lisäksi kultakin vuodelta voi tarkastella hallituksen kokoonpanoa.
-    Projekti Githubissa: https://github.com/kjoonas1/Vaalikartta.
+    return <div>
+        <h1>Tietoa sovelluksesta</h1>
+        <p>
+            Jyväskylän Yliopiston Informaatioteknologiatiedekunnan TIEA207 Aineopintojen
+            projektikurssi, jossa avointa dataa hyödyntäen on luotu web-sivusto, jossa voi katsoa
+            interaktiivisen kartan avulla Suomen eduskuntavaalien tuloksia koko maan,
+            vaalipiirien ja kuntien osalta vuodesta 1983 alkaen. Lisäksi kultakin vuodelta
+            voi tarkastella hallituksen kokoonpanoa. <a href="https://github.com/kjoonas1/Vaalikartta">Projektin lähdekoodi Githubissa</a>
+        </p>
+        <h2>Lisenssi</h2>
+        <p>
+            Tämän projektin lisenssi on GPLv3.
+            </p>
 
-    Tämän projektin lisenssi on GPLv3.
 
-    Data on haettu seuraavista lähteistä:
-    Eduskuntavaali- ja kuntatiedot. Tilastokeskus. Aineisto on ladattu Tilastokeskuksen rajapintapalvelusta lisenssillä  CC BY 4.0 https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606. Äänestystiedot ja puoluekannatus ladattiin 18.10.2019, ja  kuntien avainluvut ladattiin 31.10.2019. Data on muunnettu CSV-formaatista JSON-formaattiin ja tyhjiä kenttiä poistettu.
-    Kuntakartan tausta. OpenStreetMap. Aineisto on ladattu OpenStreetMapin rajapintapalvelusta lisenssillä CC BY-SA https://www.openstreetmap.org/copyright.
-    Kuntien koordinaatit. Wikipedia. Aineisto on haettu 22.10.2019 lisenssillä CC BY SA 4.0.
-    Kuvat. Wikipedia. Aineisto on haettu Wikipediasta lisenssillä CC BY 2.0.
+        <h2>Lähteet</h2>
+        <Table size="sm">
+            <thead>
+                <tr>
+                    <th>Lähde</th>
+                    <th>Haettu</th>
+                    <th>Lisenssi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Eduskuntavaalien äänestystiedot ja puoluekannatukset, Tilastokeskus</td>
+                    <td>18.10.2019</td>
+                    <td><a href="https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606">CC BY 4.0</a></td>
+                </tr>
+                <tr>
+                    <td>Kuntien avainluvut, Tilastokeskus</td>
+                    <td>31.10.2019</td>
+                    <td><a href="https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606">CC BY 4.0</a></td>
+                </tr>
 
-    Tekijät: Juuso Huurinainen, Aleksi Tarvainen, Joonas Kallinen, Teemu Korhonen
-    </>
+            </tbody>
+        </Table>
+        <h3>Dataan tehdyt muutokset</h3>
+        <p>
+            Tilastokeskukselta data ladattiin CSV-formaatissa ja siitä poistettiin tyhjiä kenttiä, minkä jälkeen se muutettiin JSON-formaattiin.
+            </p>
+        <p>
+            Kuntakartan tausta.OpenStreetMap.Aineisto on ladattu OpenStreetMapin
+            rajapintapalvelusta lisenssillä CC BY - SA https://www.openstreetmap.org/copyright.
+            Kuntien koordinaatit.Wikipedia.Aineisto on haettu 22.10.2019 lisenssillä CC BY SA
+            4.0. Kuvat.Wikipedia.Aineisto on haettu Wikipediasta lisenssillä CC BY 2.0.
+</p>
+
+
+        <h2>Tekijät</h2>
+        <ul>
+            <li>Joonas Kallinen</li>
+            <li>Aleksi Tarvainen</li>
+            <li>Juuso Huurinainen</li>
+            <li>Teemu Korhonen</li>
+        </ul>
+    </div>
 }
 
 export default Tietoa
