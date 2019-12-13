e424c8f8569ccb149554128b1e7610c906cadc98
Juuso <juuso45@hotmail.com>
2019-12-13 14:44:00 +0200

    Aloitettu tietoa-sivun tekoa

--- vaalikartta-frontend/src/components/Tietoa.jsx
+++ vaalikartta-frontend/src/components/Tietoa.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const Tietoa = () => {
-    return <>Tietoa</>
+    return <>Jyväskylän Yliopiston Informaatioteknologiatiedekunnan TIEA207-kurssin projekti, jossa avointa dataa hyödyntäen on luotu web-sivusto, jossa voi katsoa interaktiivisen kartan avulla Suomen eduskuntavaalien tuloksia koko maan, vaalipiirien ja kuntien osalta vuodesta 1983 alkaen. Lisäksi kultakin vuodelta voi tarkastella hallituksen kokoonpanoa.
+    Projekti Githubissa: https://github.com/kjoonas1/Vaalikartta.
+
+    Tämän projektin lisenssi on GPLv3.
+
+    Data on haettu seuraavista lähteistä:
+    Eduskuntavaali- ja kuntatiedot. Tilastokeskus. Aineisto on ladattu Tilastokeskuksen rajapintapalvelusta .10.2019 lisenssillä  CC BY 4.0 https://creativecommons.org/licenses/by/4.0/deed.fi?_ga=2.218811536.880534282.1576159684-1137257823.1571741606.
+    Kuntakartan tausta. OpenStreetMap. Aineisto on ladattu OpenStreetMapin rajapintapalvelusta  .2019 lisenssillä CC BY-SA https://www.openstreetmap.org/copyright.
+    Kuntien koordinaatit. Wikipedia. Aineisto on haettu .2019 lisenssillä CC BY SA 4.0.
+     </>
 }
 
 export default Tietoa
